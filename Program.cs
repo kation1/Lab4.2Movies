e349a9f66fe89f17f62bb7a6e597ddb5b4754931
Kathryn Pidek <KathrynPidek@quickenloans.com>
2020-07-27 02:15:49 -0400

    added add movie option

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+
 
 namespace Lab4._2Movies
 {
@@ -20,7 +20,6 @@ namespace Lab4._2Movies
             get
             {
                 return _genre;
-
             }
         }
         public Movie(string thetitle, string thegenre)
@@ -32,6 +31,28 @@ namespace Lab4._2Movies
         {
             return Title;
         }
+
+        public static void AddMovie(List<Movie> movies)
+        {
+
+            Console.WriteLine("What is the name of the movie?");
+            string movieName = Console.ReadLine();
+            Console.WriteLine("What is the movie's genre?");
+            string movieGenre = Console.ReadLine();
+            Movie newMovie = new Movie(movieName, movieGenre);
+            movies.Add(newMovie);
+        }
+        public static void GenreList(List<Movie> movies)
+        { string genreName = "";
+            foreach(Movie title in movies)
+            {   if (title._genre != genreName)
+                {
+                    Console.WriteLine(title._genre);
+                    genreName = title._genre;
+                }
+            
+            }
+        }
     }
     class Program
     {
@@ -40,7 +61,7 @@ namespace Lab4._2Movies
             List<Movie> movies = new List<Movie>();
             Movie newMovie = new Movie("Casino Royale", "Action");
             movies.Add(newMovie);
-            movies.Add(newMovie = new Movie ("Die Hard", "Action"));
+            movies.Add(newMovie = new Movie("Die Hard", "Action"));
             movies.Add(newMovie = new Movie("King Kong", "Adventure"));
             movies.Add(newMovie = new Movie("Jurassic Park", "Adventure"));
             movies.Add(newMovie = new Movie("The Lord of the Rings", "Fantasy"));
@@ -51,18 +72,45 @@ namespace Lab4._2Movies
             movies.Add(newMovie = new Movie("Spirited Away", "Animated"));
 
             string userContinue = "on";
+            string userInput = "";
             do
             {
-                Console.WriteLine("What kind of movie do you want to look at?");
-                string userGenre = Console.ReadLine();
-
-                foreach (Movie movie in movies)
+                Console.WriteLine("Do you want to search or add a new movie? (add/search)");
+                userInput = Console.ReadLine();
+                if (userInput == "add")
                 {
-                    if (movie.Genre.Contains(userGenre))
-                        Console.WriteLine(movie.Title);
+                    Movie.AddMovie(movies);
                 }
-                Console.WriteLine("Do you want to continue? Enter 'q' to quit or enter to continue");
-               userContinue = Console.ReadLine();
+                else if (userInput == "search")
+                {
+
+                    Console.WriteLine("What kind of movie do you want to look at?"); 
+                    Console.WriteLine($"Currently we have options in these genres");
+                    Movie.GenreList(movies);
+                    string userGenre = Console.ReadLine();
+
+                    int count = 1;
+                    bool itemFound = false;
+                    foreach (Movie movie in movies)
+                    {
+
+                        if (movie.Genre.Contains(userGenre))
+                        {
+                            Console.Clear();
+                            Console.WriteLine($"List of movies in {userGenre}:");
+                            Console.WriteLine(movie.Title);
+                            itemFound = true;
+
+                        }
+                        else if (count == movies.Count && itemFound == false)
+                        {
+                            Console.WriteLine($"\nThere were no movies in that genre.\n\n");
+                        }
+                        count++;
+                    }
+                }
+                Console.WriteLine($"\n\n\n\nDo you want to continue? Enter 'q' to quit or enter to continue");
+                userContinue = Console.ReadLine();
             }
             while (userContinue != "q");
 
